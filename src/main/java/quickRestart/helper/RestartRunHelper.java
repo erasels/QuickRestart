a2b450adce7f7710bd166fb83a07b93825878da8
erasels <emanuelbedburdick@yahoo.de>
2019-07-16 08:04:44 +0200

    Removed duplicate code and reset merchant card purge cost

--- src/main/java/quickRestart/helper/RestartRunHelper.java
+++ src/main/java/quickRestart/helper/RestartRunHelper.java
@@ -7,20 +7,22 @@ import com.megacrit.cardcrawl.dungeons.Exordium;
 import com.megacrit.cardcrawl.helpers.SeedHelper;
 import com.megacrit.cardcrawl.random.Random;
 import com.megacrit.cardcrawl.screens.DungeonTransitionScreen;
+import com.megacrit.cardcrawl.shop.ShopScreen;
 
 public class RestartRunHelper {
     public static void restartRun() {
         CardCrawlGame.music.fadeAll();
         AbstractDungeon.getCurrRoom().clearEvent();
         AbstractDungeon.closeCurrentScreen();
-        AbstractDungeon.dungeonMapScreen.closeInstantly();
+        //AbstractDungeon.dungeonMapScreen.closeInstantly();
         CardCrawlGame.dungeonTransitionScreen = new DungeonTransitionScreen(Exordium.ID);
-        AbstractDungeon.rs = AbstractDungeon.RenderScene.NORMAL;
+        //AbstractDungeon.rs = AbstractDungeon.RenderScene.NORMAL;
 
         AbstractDungeon.reset();
         Settings.hasEmeraldKey = false;
         Settings.hasRubyKey = false;
         Settings.hasSapphireKey = false;
+        ShopScreen.resetPurgeCost();
 
         if (CardCrawlGame.chosenCharacter == null) {
             CardCrawlGame.chosenCharacter = AbstractDungeon.player.chosenClass;
